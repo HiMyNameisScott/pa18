30b6ec210f3597f4f6a65e2d8df1a4701dd9011b
Scott T. Koss <Scott.T.Koss@gmail.com>
2022-11-22 05:36:26 -0800

    more fixes

--- pa18.cpp
+++ pa18.cpp
@@ -26,9 +26,9 @@ public:
 ///============================================================================
 int precedence(char ch);
 void obtain_data(std::string &infix);
-void process_infix(std::string& final, 
-                   Stack<char>& stack, 
-                   std::string& evaluate);
+void process_infix(std::string& postfix_exp, 
+                   Stack<char>& post_operators, 
+                   std::string& infix);
 
 ///============================================================================
 /// MAIN=======================================================================
@@ -36,23 +36,23 @@ void process_infix(std::string& final,
 
 int main() {
 
-    char add_to_postfix;        //add to postfix expression                                                                                                                                                                                                                                                                                                                                                                                                                                         
-    std::string postfix_exp;    //stores postfix expression
-    Stack<char> post_operators; //stack to store operands
-    std::string infix;          //stores characters from input
-    int count = 0;              //tracks the number of times to iterate loop
+    char add_to_postfix;        // add to postfix expression                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    std::string postfix_exp;    // stores postfix expression
+    Stack<char> post_operators; // stack to store operands
+    std::string infix;          // stores characters from input
+    int count = 0;              // tracks the number of times to iterate loop
+    std::string number_of_its;  // stores the number of times to perform process
 
-    std::string number_of_its;
-    std::getline(std::cin, number_of_its);
-    std::cin.ignore();
+
+    std::getline(std::cin, number_of_its);  // grabs number of iterations
+    std::cin.ignore();                      // ignores first \n
 
     while (count < stoi(number_of_its)){
         obtain_data(infix);
         process_infix(postfix_exp, post_operators, infix);
-
         count ++;
-        }
-    std::cout << "infix" << infix << std::endl;
+    }
+
 return 0;
 }
 
@@ -60,65 +60,66 @@ return 0;
 /// Function Definitions for pa18==============================================
 ///============================================================================
 
-/// @brief 
-/// @param postfix_exp 
-/// @param post_operators 
-/// @param text 
-void process_infix(std::string& final, 
-                Stack<char>& stack, 
-                std::string& evaluate){
-    for (char ch : evaluate){               // evaluate each ch in text array
-        if (ch >= 48 && ch <= 56){      // if the char is between 0-9
-            final.push_back(ch);  // add directly to expression
-        } else if (ch == '(') {         // if open parans we can push this to operator stack
-            stack.push(ch);
-        } else if (ch == ')'){          //
-            while (!stack.empty()){    // While the stack is not empty, we can push to expression
-                char ch = stack.top();        // ch = the top of the stack
-                final.push_back(ch);             // push to expression
-                stack.pop();                // pop the top of the stack, repeat until empty
+/// @brief performs infix to postfix conversion
+/// @param postfix_exp accesses string to print postfix
+/// @param post_operators access stack to store operators
+/// @param text access string of input from file to process
+void process_infix(std::string& postfix_exp, 
+                Stack<char>& post_operators, 
+                std::string& infix){
+    for (char ch : infix){     // evaluate each ch in text array
+        if (ch >= '0' && ch <= '9'){ // if the char is between 0-9
+            postfix_exp.push_back(ch);  // add directly to expression
+        } else if (ch == '(') {   // if open parans we can push this to operator stack
+            post_operators.push(ch);
+        } else if (ch == ')'){          
+            while (!post_operators.empty()){ // While the stack is not empty, we can push to expression
+                char ch = post_operators.top(); // ch = the top of the stack
+                postfix_exp.push_back(ch);   // push to expression
+                post_operators.pop();           // pop the top of the stack, repeat until empty
             }
         } else { 
-            while ( stack.empty() != true &&   // loop while stack is not empty
-                    precedence(ch) <= precedence(stack.top()) ){ // if the precedence of the operator is < ****
-                        char ch = stack.top(); // set char to push
-                        final.push_back(ch);      // push the char to expression
-                        stack.pop();            // pop char from the stack
+            while ( post_operators.empty() != true && // loop while stack is not empty
+                    precedence(ch) <= precedence(post_operators.top()) ){ // if the precedence of the operator is < ****
+                        char ch = post_operators.top(); // set char to push
+                        postfix_exp.push_back(ch); // push the char to expression
+                        post_operators.pop(); // pop char from the stack
             }
         }
-        while (!stack.empty()){    // No other test cases, add rest
-            char ch = stack.top();
-            final.push_back(ch);
-            stack.pop();
+        while (!post_operators.empty()){ // No other test cases, add rest
+            char ch = post_operators.top();
+            postfix_exp.push_back(ch);
+            post_operators.pop();
     }
-
-        std::cout << final ;           // print out the expression
+        
     }
+    std::cout << postfix_exp << std::endl; // print out the expression
 };
 
 /// @brief This will return the precedence of the char operand
 /// @param ch taken in to check for precedence
 /// @return the precedence of the operand
 int precedence(char ch) {
+    int num = 0;
      switch(ch){
-        case '+': return 1; break;
-        case '-': return 1; break;
-        case '*': return 2; break;
-        case '/': return 2; break;
-        default: return 0; break;
+        case '+': num = 1; break;
+        case '-': num = 1; break;
+        case '*': num = 2; break;
+        case '/': num = 2; break;
+        default: num = 0; break;
      } 
+     return num;
 }
 
 
-/// @brief 
-/// @param infix 
-void obtain_data(std::string &infix){
-    
-    while(std::cin.peek() != 10){
-        std::string line;
-        getline(std::cin, line);
-        std::cin.ignore(-1, '\n');
-        char ch = line[0];
-        infix.push_back(ch);
-    }
+/// @brief pushes data to a string to process for in-->post
+/// @param input is a string to process
+void obtain_data(std::string& infix){
+    char ch_l;
+    do {
+        std::string line; // create new string
+        std::getline(std::cin, line); // get the string
+        char ch_l = line[0];  // take the first char for infix
+        infix.push_back(ch_l); // push char to infix string
+    } while (std::cin.peek() != -1); // loop condition :)
 };
